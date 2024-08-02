877559820c1a219169d65323e4c6bb4ab13b090a
Akkshay Tandon <76265879+akkshayTandon@users.noreply.github.com>
2024-08-03 02:28:56 +0530

    Delete src/components directory

--- src/components/DomainIcon.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-
-export default function DomainIcon({height, width, viewBox, path}){
-    return (
-        <svg className="ml-2 mr-2" xmlns="http://www.w3.org/2000/svg" height={height} width={width} viewBox={viewBox}><path fill="#c2410c" d={path} /></svg>
-    )
-}
